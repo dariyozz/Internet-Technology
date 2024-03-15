f6fdf522fab3a850b9f51cdd91538a41106f0dc3
Dario <dariojakovleski10@gmail.com>
2024-03-15 10:47:55 +0100

    Add lab 1

---
+++ LAB1/Internet_Tehnologii_Lab1_222015/Internet_Tehnologii_Lab1_222015/Najava.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Internet_Tehnologii_Lab1_222015
+{
+    public partial class Najava : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid) {
+                Response.Redirect("Glasaj.aspx");
+            }
+        }
+
+    }
+}
